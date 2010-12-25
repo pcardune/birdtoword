0d85f8b15b49ec4b1585e94bb7b9a257eb7fc683
pcarduner <pcardune@facebook.com>
2010-12-24 22:33:25 -0800

    get game history to display

--- node/serve.js
+++ node/serve.js
@@ -10,46 +10,40 @@ var app = express.createServer(
   express.bodyDecoder()
 );
 
-app.configure(
-  function(){
-    app.use(express.cookieDecoder());
-    app.use(express.session({store:new MemoryStore({reapInterval: 60000 * 10 })}));
-    app.use(express.methodOverride());
-    app.use(express.bodyDecoder());
-    app.use(app.router);
-    app.use(express.staticProvider(__dirname + '/../app/src/ui'));
-  });
+app.configure(function(){
+  app.use(express.cookieDecoder());
+  app.use(express.session({store:new MemoryStore({reapInterval: 60000 * 10 })}));
+  app.use(express.methodOverride());
+  app.use(express.bodyDecoder());
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/../app/src/ui'));
+});
 
-app.configure(
-  'development',
-  function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  });
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-app.configure(
-  'production',
-  function(){
-    app.use(express.errorHandler());
-  });
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
 app.set('view engine', 'jade');
 
-app.get(
-  '/',
-  function(req, res) {
-    res.render('index', {
-                 locals: {scripts: ['front-page.js']}
-               });
+app.get('/', function(req, res) {
+  res.render('index', {
+    locals: {
+      scripts: ['front-page.js'],
+      games: req.session.games || []
+    }
   });
+});
 
-app.get(
-  /\/api\/game-words\/([345])/,
-  function(req, res) {
-    var size = parseInt(req.params[0]);
-    graph.getBucket(size, function(bucket) {
-      res.send(bucket.getPath());
-    });
+app.get(/\/api\/game-words\/([345])/, function(req, res) {
+  var size = parseInt(req.params[0]);
+  graph.getBucket(size, function(bucket) {
+    res.send(bucket.getPath());
   });
+});
 
 app.get(/\/api\/words\/(.*)\/connected/, function(req, res) {
   var word = req.params[0];
@@ -59,22 +53,24 @@ app.get(/\/api\/words\/(.*)\/connected/, function(req, res) {
 });
 
 app.post('/api/save-game', function(req, res) {
-  var
-    words = req.param("words"),
-    times = req.param("times");
+  var words = req.param("words"),
+      times = req.param("times");
 
   if (!req.session.games) {
     req.session.games = [];
   }
 
-  req.session.games.push({
+  var game = {
     fromWord: words[0],
     toWord: _.last(words),
     words: words.slice(1, words.length-1),
-    times: [times.map(function(t){ return parseFloat(t); })]
-  });
+    times: times.map(function(t){ return parseFloat(t); })
+  };
+  game.time = _.reduce(game.times, function(memo, num){ return memo + num; }, 0);
+  game.changes = game.words.length+1;
 
-  res.send(null);
+  req.session.games.push(game);
+  res.send(game);
 });
 
 var port = 3000;
